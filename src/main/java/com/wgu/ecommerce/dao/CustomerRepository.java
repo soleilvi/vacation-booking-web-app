f279b34afc7cffe227dab8526c978351af03917f
soleilvi <soleil.vivero@gmail.com>
2024-09-14 12:56:49 -0700

    Section E.1: added cross-origin support to my repositories

--- src/main/java/com/wgu/ecommerce/dao/CustomerRepository.java
+++ src/main/java/com/wgu/ecommerce/dao/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.wgu.ecommerce.dao;
 import com.wgu.ecommerce.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "Customer", path = "customers")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
