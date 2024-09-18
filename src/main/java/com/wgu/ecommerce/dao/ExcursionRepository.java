cf58c90cadf3b8b6db35e8c44be1c042a940ea9d
soleilvi <soleil.vivero@gmail.com>
2024-09-18 08:29:45 -0700

    Section E.2: StatusType cleanup and cross-origin re-enabled, mapping works correctly now

--- src/main/java/com/wgu/ecommerce/dao/ExcursionRepository.java
+++ src/main/java/com/wgu/ecommerce/dao/ExcursionRepository.java
@@ -4,6 +4,6 @@ import com.wgu.ecommerce.entities.Excursion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 public interface ExcursionRepository extends JpaRepository<Excursion, Long> {
 }
--- src/main/java/com/wgu/ecommerce/entities/StatusType.java
+++ src/main/java/com/wgu/ecommerce/entities/StatusType.java
@@ -1,25 +1,5 @@
 package com.wgu.ecommerce.entities;
 
 public enum StatusType {
-    pending, ordered, canceled;
-//    pending("p"), ordered("o"), canceled("c");
-//
-//    private String shortName;
-//
-//    private StatusType(String shortName) { this.shortName = shortName; }
-//
-//    public String getShortName() { return shortName; }
-//
-//    public static StatusType fromShortName(String shortName) {
-//        switch (shortName) {
-//            case "p":
-//                return StatusType.pending;
-//            case "o":
-//                return StatusType.ordered;
-//            case "c":
-//                return StatusType.canceled;
-//            default:
-//                throw new IllegalArgumentException("Unknown status type: " + shortName);
-//        }
-//    }
+    pending, ordered, canceled;  // Typo in "cancelled" because that's how it is spelled in the front-end template
 }
