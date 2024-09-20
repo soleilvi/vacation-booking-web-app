c104a87c7757c2126a7a403608b6b4991e1c63c0
soleilvi <soleil.vivero@gmail.com>
2024-09-19 18:36:41 -0700

    Section G.1: Fixed division validation that prevented the order tracking number from showing up

--- src/main/java/com/wgu/ecommerce/entities/Customer.java
+++ src/main/java/com/wgu/ecommerce/entities/Customer.java
@@ -44,7 +44,7 @@ public class Customer {
     private Date last_update;
 
     @ManyToOne
-    @JoinColumn(name = "division_id", nullable = false)
+    @JoinColumn(name = "division_id")
     private Division division;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "id")
