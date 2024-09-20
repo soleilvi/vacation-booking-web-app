e3ffd75fdf8465de91aa7fb0cdb4e65ba197df1d
soleilvi <soleil.vivero@gmail.com>
2024-09-20 07:42:57 -0700

    Section G.2: Experimenting with @NotEmpty validation for cart at checkout

--- src/main/java/com/wgu/ecommerce/dto/Purchase.java
+++ src/main/java/com/wgu/ecommerce/dto/Purchase.java
@@ -3,6 +3,7 @@ package com.wgu.ecommerce.dto;
 import com.wgu.ecommerce.entities.Cart;
 import com.wgu.ecommerce.entities.CartItem;
 import com.wgu.ecommerce.entities.Customer;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.Set;
@@ -14,5 +15,6 @@ public class Purchase {
 
     private Cart cart;
 
+    @NotEmpty(message = "Cart is empty, please add an item before checking out.")
     private Set<CartItem> cartItems;
 }
